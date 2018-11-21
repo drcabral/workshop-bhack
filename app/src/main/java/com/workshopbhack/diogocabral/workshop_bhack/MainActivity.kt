6e1e6d5c0e7b6e452f07c25cf838e3c3c025bce3
Diogo Cabral <drkbral@gmail.com>
2018-11-21 00:30:00 -0200

    Adding base project.

---
+++ app/src/main/java/com/workshopbhack/diogocabral/workshop_bhack/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.workshopbhack.diogocabral.workshop_bhack
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
