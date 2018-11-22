3e4fd1b8c90e6d3f1ef62b38fb52ec55951de898
marcellasouza <marcellalcs@gmail.com>
2018-11-22 13:47:14 -0200

    Step 2 - Adding action to button

--- app/src/main/java/com/workshopbhack/diogocabral/workshop_bhack/MainActivity.kt
+++ app/src/main/java/com/workshopbhack/diogocabral/workshop_bhack/MainActivity.kt
@@ -2,11 +2,19 @@ package com.workshopbhack.diogocabral.workshop_bhack
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
+    var clicked = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        generatePhrase.setOnClickListener{
+            clicked++
+            generatedPhrase.text = "fui clicado! $clicked"
+        }
+
     }
 }
