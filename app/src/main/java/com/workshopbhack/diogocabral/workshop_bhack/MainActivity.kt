0fd65bcbc71c9debe5b32bb7967c7c1b0bc1aa08
marcellasouza <marcellalcs@gmail.com>
2018-11-23 17:12:32 -0200

    Step 2 - Adding toast and new layout to step 2

--- app/src/main/java/com/workshopbhack/diogocabral/workshop_bhack/MainActivity.kt
+++ app/src/main/java/com/workshopbhack/diogocabral/workshop_bhack/MainActivity.kt
@@ -2,6 +2,8 @@ package com.workshopbhack.diogocabral.workshop_bhack
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -9,6 +11,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val myToast = Toast.makeText(applicationContext, "Minha primeira toast", Toast.LENGTH_SHORT)
+        myToast.setGravity(Gravity.CENTER, Gravity.CENTER, 0)
+
+        toastButton.setOnClickListener({
+            myToast.show()
+        })
 
 
         generatePhrase.setOnClickListener{
